6c9f7c0812932e28c3f875e3d8ffef0ce0a5ec67
SwimLurker <SwimLurker@gmail.com>
2015-10-21 23:10:16 +0800

    modify for v2 ui

---
+++ HsinchuIOT/src/org/slstudio/hsinchuiot/V2LoginActivity.java
@@ -0,0 +1,312 @@
+package org.slstudio.hsinchuiot;
+
+import java.util.TimeZone;
+
+import org.slstudio.hsinchuiot.model.Session;
+import org.slstudio.hsinchuiot.model.User;
+import org.slstudio.hsinchuiot.service.IOTException;
+import org.slstudio.hsinchuiot.service.LoginService;
+import org.slstudio.hsinchuiot.service.ServiceContainer;
+import org.slstudio.hsinchuiot.service.http.ForgroundRequestListener;
+import org.slstudio.hsinchuiot.service.http.HttpConfig;
+import org.slstudio.hsinchuiot.service.http.HttpRequest;
+import org.slstudio.hsinchuiot.service.http.NoneAuthedHttpRequest;
+import org.slstudio.hsinchuiot.service.http.RequestControl;
+import org.slstudio.hsinchuiot.service.http.RequestListener;
+import org.slstudio.hsinchuiot.util.EncryptUtil;
+
+import android.app.ActionBar;
+import android.content.Context;
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.KeyEvent;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.EditText;
+
+public class V2LoginActivity extends BaseActivity {
+
+	private Object lock = new Object();
+
+	private EditText usernameET;
+	private EditText passwordET;
+	private Button signInBtn;
+	private GetSessionIDListener listener;
+	private LoginListener listener2;
+	private Handler handler;
+
+	private String sessionID;
+	private String username;
+	private String password;
+	private User loginUser;
+
+	private boolean succeed;
+	private IOTException exception;
+
+	private CheckBox rememberPassword;
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.common_login_v2);
+		
+		
+		usernameET = (EditText) findViewById(R.id.login_username);
+
+		passwordET = (EditText) findViewById(R.id.login_password);
+		signInBtn = (Button) findViewById(R.id.login_signin);
+		signInBtn.setOnClickListener(new OnClickListener() {
+
+			@Override
+			public void onClick(View v) {
+				doSignIn();
+				// gotoMainScreen();
+			}
+		});
+
+		rememberPassword = (CheckBox) findViewById(R.id.remeber_password);
+
+		listener = new GetSessionIDListener(this, true, getString(R.string.common_please_wait));
+		listener2 = new LoginListener();
+		handler = new Handler();
+		ServiceContainer.getInstance().getUpgradeController().checkVersion(null, true);
+		
+	}
+
+	protected void doSignIn() {
+
+		username = usernameET.getText().toString().trim();
+		password = passwordET.getText().toString().trim();
+
+		if (username.equals("")) {
+			setException(new IOTException(-1, getString(R.string.error_message_require_loginname_text)));
+			showDialog(DIALOG_ERROR);
+			return;
+		}
+
+		if (password.equals("")) {
+			setException(new IOTException(-1, getString(R.string.error_message_require_pwd_text)));
+			showDialog(DIALOG_ERROR);
+			return;
+		}
+
+		HttpRequest request = new NoneAuthedHttpRequest(new HttpConfig.GetHttpConfig(),
+				Constants.ServerAPIURI.GET_SESSION_ID);
+
+		request.addParameter("dataType", "xml");
+
+		ServiceContainer.getInstance().getHttpHandler().doRequest(request, listener);
+
+	}
+
+	protected void gotoNormalUserMainScreen() {
+		Intent intent = new Intent(Constants.Action.HSINCHUIOT_USER_MAIN);
+		startActivity(intent);
+		finish();
+	}
+
+	protected void gotoAdminUserMainScreen() {
+		Intent intent = new Intent(Constants.Action.HSINCHUIOT_SUPERUSER_MAIN);
+		startActivity(intent);
+		finish();
+	}
+
+	@Override
+	protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+		super.onActivityResult(requestCode, resultCode, data);
+		if (resultCode == 1) {
+			finish();
+		}
+	}
+
+	public boolean onKeyDown(int keyCode, KeyEvent event) {
+		if (keyCode == KeyEvent.KEYCODE_BACK) {
+			moveTaskToBack(true);
+			return true;
+		}
+		return super.onKeyDown(keyCode, event);
+	}
+
+	private class GetSessionIDListener extends ForgroundRequestListener<Session> {
+		private RequestControl control;
+
+		public GetSessionIDListener(Context context, boolean isShowProgressDialog, String content) {
+			super(context, isShowProgressDialog, content);
+		}
+
+		@Override
+		public void onRequestCancelled() {
+			if (control != null)
+				control.cancel();
+
+		}
+
+		@Override
+		public void onRequestResult(final Session result) {
+			sessionID = result.getSessionID();
+
+			String pwdMD5 = EncryptUtil.getStringMD5(password);
+
+			String mangledPwd = EncryptUtil.getStringMD5(pwdMD5 + ":" + sessionID);
+
+			HttpRequest request2 = new NoneAuthedHttpRequest(new HttpConfig.GetHttpConfig(),
+					Constants.ServerAPIURI.LOGIN);
+
+			request2.addParameter("dataType", "json");
+			request2.addParameter("__session_id", sessionID);
+			request2.addParameter("username", username);
+			request2.addParameter("mangled_password", mangledPwd);
+			request2.addParameter("lang", "zh-cn");
+			request2.addParameter("timezone", Integer.toString(TimeZone.getDefault().getRawOffset()));
+			ServiceContainer.getInstance().getHttpHandler().doRequest(request2, listener2);
+			synchronized (lock) {
+				try {
+					lock.wait();
+				} catch (InterruptedException e1) {
+					// TODO Auto-generated catch block
+					e1.printStackTrace();
+				}
+
+			}
+
+			if (!succeed) {
+				onRequestError(exception);
+				return;
+			}
+
+			if (rememberPassword.isChecked()) {
+				ServiceContainer.getInstance().getPerferenceService().setValue(V2LoginActivity.this,
+						Constants.PreferenceKey.LOGINNAME, username);
+
+				ServiceContainer.getInstance().getPerferenceService().setValue(V2LoginActivity.this,
+						Constants.PreferenceKey.PASSWORD, EncryptUtil.getStringMD5(password));
+			} else {
+				ServiceContainer.getInstance().getPerferenceService().setValue(V2LoginActivity.this,
+						Constants.PreferenceKey.LOGINNAME, null);
+
+				ServiceContainer.getInstance().getPerferenceService().setValue(V2LoginActivity.this,
+						Constants.PreferenceKey.PASSWORD, null);
+			}
+
+			ServiceContainer.getInstance().getSessionService().setSessionID(sessionID);
+
+			loginUser.setLoginName(username);
+			loginUser.setPassword(pwdMD5);
+			ServiceContainer.getInstance().getSessionService().setLoginUser(loginUser);
+
+			ServiceContainer.getInstance().getSessionService().setSessionValue(Constants.SessionKey.THRESHOLD_WARNING,
+					LoginService.getWarningThreshold(V2LoginActivity.this));
+			ServiceContainer.getInstance().getSessionService().setSessionValue(Constants.SessionKey.THRESHOLD_BREACH,
+					LoginService.getBreachThreshold(V2LoginActivity.this));
+
+			if(loginUser.isNormalUser()){
+				int refreshTime = 10;
+				String refreshTimeStr = ServiceContainer.getInstance().getPerferenceService().getValue(V2LoginActivity.this, Constants.PreferenceKey.REALTIME_DATA_MONITOR_REFRESH_TIME);
+				if(!"".equals(refreshTimeStr)){
+					refreshTime = Integer.parseInt(refreshTimeStr);
+				}
+				
+				ServiceContainer.getInstance().getSessionService().setSessionValue(Constants.SessionKey.REALTIME_DATA_MONITOR_REFRESH_TIME, refreshTime);
+			}
+			
+			handler.post(new Runnable() {
+
+				@Override
+				public void run() {
+					/*
+					 * username.setText(ServiceContainer.getInstance ()
+					 * .getPerferenceService()
+					 * .getSessionId(LoginActivity.this));
+					 */
+					if (loginUser.isAdminUser()) {
+						gotoAdminUserMainScreen();
+					} else if (loginUser.isNormalUser()){
+						gotoNormalUserMainScreen();
+					}else{
+						setException(new IOTException(-1, getString(R.string.error_message_user_permission_wrong)));
+						showDialog(DIALOG_ERROR);
+						return;
+					}
+				}
+			});
+
+		}
+
+		@Override
+		public void onRequestGetControl(RequestControl control) {
+			this.control = control;
+		}
+	}
+
+	private class LoginListener implements RequestListener<User> {
+		private RequestControl control;
+
+		@Override
+		public void onRequestCancelled() {
+			if (control != null)
+				control.cancel();
+
+		}
+
+		@Override
+		public void onRequestResult(final User result) {
+			succeed = true;
+			loginUser = result;
+		}
+
+		@Override
+		public void onRequestGetControl(RequestControl control) {
+			this.control = control;
+		}
+
+		@Override
+		public void onRequestStart() {
+			// TODO Auto-generated method stub
+
+		}
+
+		@Override
+		public void onRequestError(Exception e) {
+			succeed = false;
+			if (e instanceof IOTException) {
+				exception = (IOTException) e;
+			} else {
+				exception = new IOTException(-1, e.getMessage());
+			}
+		}
+
+		@Override
+		public void onRequestComplete() {
+			synchronized (lock) {
+				lock.notify();
+			}
+		}
+	}
+
+	protected void setupActionBar() {
+		ActionBar actionBar = getActionBar();
+		actionBar.setHomeButtonEnabled(true);
+		actionBar.setTitle(R.string.common_sign_in);
+		super.setupActionBar();
+	}
+
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+		final int btnId = item.getItemId();
+		if (btnId == android.R.id.home) {
+			finish();
+		}
+		return true;
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		return true;
+	}
+
+}
---
+++ HsinchuIOT/src/org/slstudio/hsinchuiot/V2SplashActivity.java
@@ -0,0 +1,166 @@
+package org.slstudio.hsinchuiot;
+
+import java.io.File;
+
+import org.slstudio.hsinchuiot.model.User;
+import org.slstudio.hsinchuiot.service.IOTException;
+import org.slstudio.hsinchuiot.service.LoginService;
+import org.slstudio.hsinchuiot.service.ServiceContainer;
+import org.slstudio.hsinchuiot.util.ImageUtil;
+
+import android.app.ProgressDialog;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.content.DialogInterface.OnCancelListener;
+import android.graphics.Bitmap;
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.Window;
+
+public class V2SplashActivity extends BaseActivity {
+
+	private final int SPLASH_DISPLAY_LENGTH = 1000;
+	private ProgressDialog progressDialog;
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		requestWindowFeature(Window.FEATURE_NO_TITLE);
+		setContentView(R.layout.activity_splash_v2);
+
+		new Handler().postDelayed(new Runnable() {
+
+			@Override
+			public void run() {
+				// show progress dialog in UI thread
+				showProgressDialog();
+				// start new thread for handle the login
+				new Thread(new Runnable() {
+
+					@Override
+					public void run() {
+						prepare();
+						login();
+						//showDebugActivity(Constants.Action.HSINCHUIOT_USER_CHART_SETTINGS);
+					}
+
+				}).start();
+
+			}
+
+		}, SPLASH_DISPLAY_LENGTH);
+
+	}
+
+	private void showProgressDialog() {
+		progressDialog = ProgressDialog.show(V2SplashActivity.this, "", getString(R.string.common_please_wait), true);
+		progressDialog.setProgressStyle(ProgressDialog.STYLE_SPINNER);
+		progressDialog.setCanceledOnTouchOutside(false);
+		progressDialog.setCancelable(true);
+		progressDialog.setOnCancelListener(new OnCancelListener() {
+
+			@Override
+			public void onCancel(DialogInterface dialog) {
+			}
+		});
+	}
+
+	private void showLoginActivity() {
+		Intent i = new Intent(Constants.Action.HSINCHUIOT_LOGIN);
+		i.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+		i.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+		startActivity(i);
+		progressDialog.dismiss();
+		finish();
+
+	}
+
+	protected void gotoNormalUserMainScreen() {
+		Intent intent = new Intent(Constants.Action.HSINCHUIOT_USER_MAIN);
+		startActivity(intent);
+		progressDialog.dismiss();
+		finish();
+	}
+
+	protected void gotoAdminUserMainScreen() {
+		Intent intent = new Intent(Constants.Action.HSINCHUIOT_SUPERUSER_MAIN);
+		startActivity(intent);
+		progressDialog.dismiss();
+		finish();
+	}
+	
+	private void showDebugActivity(String action){
+		Intent intent = new Intent(action);
+		startActivity(intent);
+		progressDialog.dismiss();
+		finish();
+	}
+
+	private void prepare() {
+		//prepareThumbnailImage();
+		ServiceContainer.getInstance().getUpgradeController().resetFirstTimeCheckFlag();
+	}
+
+	private void prepareThumbnailImage() {
+		String imageDir = Constants.ImageLoader.IMAGE_ENGINE_CACHE;
+		String siteImageFilename = "site_59.png";
+		String thumbnailDir = imageDir + "/thumbnail";
+
+		File dir = new File(thumbnailDir);
+		if (!dir.exists()) {
+			dir.mkdirs();
+		}
+
+		Bitmap siteImage = ImageUtil.getBitmapFromResource(this.getResources(), R.drawable.site_a);
+		ImageUtil.writeBitmapToFile(siteImage, imageDir + "/" + siteImageFilename);
+
+		Bitmap thumbnailImage = ImageUtil.getImageThumbnail(imageDir + "/" + siteImageFilename, 128, 128);
+		ImageUtil.writeBitmapToFile(thumbnailImage, thumbnailDir + "/" + siteImageFilename);
+
+	}
+
+	private void login() {
+		String loginName = ServiceContainer.getInstance().getPerferenceService().getValue(V2SplashActivity.this,
+				Constants.PreferenceKey.LOGINNAME);
+		String password = ServiceContainer.getInstance().getPerferenceService().getValue(V2SplashActivity.this,
+				Constants.PreferenceKey.PASSWORD);
+
+		if (loginName.equals("") || password.equals("")) {
+			showLoginActivity();
+		} else {
+			try {
+				if (LoginService.getInstance().login(loginName, password)) {
+					ServiceContainer.getInstance().getSessionService().setSessionValue(Constants.SessionKey.THRESHOLD_WARNING,
+							LoginService.getWarningThreshold(this));
+					ServiceContainer.getInstance().getSessionService().setSessionValue(Constants.SessionKey.THRESHOLD_BREACH,
+							LoginService.getBreachThreshold(this));
+					User loginUser = ServiceContainer.getInstance().getSessionService().getLoginUser();
+
+					if (loginUser == null) {
+						showLoginActivity();
+					} else {
+						if (loginUser.isAdminUser()) {
+							gotoAdminUserMainScreen();
+						} else if(loginUser.isNormalUser()) {
+							int refreshTime = 10;
+							String refreshTimeStr = ServiceContainer.getInstance().getPerferenceService().getValue(this, Constants.PreferenceKey.REALTIME_DATA_MONITOR_REFRESH_TIME);
+							if(!"".equals(refreshTimeStr)){
+								refreshTime = Integer.parseInt(refreshTimeStr);
+							}
+							
+							ServiceContainer.getInstance().getSessionService().setSessionValue(Constants.SessionKey.REALTIME_DATA_MONITOR_REFRESH_TIME, refreshTime);
+							gotoNormalUserMainScreen();
+						}else{
+							showLoginActivity();
+						}
+					}
+				} else {
+					showLoginActivity();
+				}
+			} catch (IOTException e) {
+				showLoginActivity();
+			}
+
+		}
+	}
+}
